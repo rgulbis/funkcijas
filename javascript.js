0cc4d3f1c92561c87c38a69719557af5d66bdfa0
rgulbis <rgubis41@gmail.com>
2025-12-15 14:56:07 +0200

    smukums

--- javascript.js
+++ javascript.js
@@ -8,21 +8,18 @@ console.log(kvadrata(7));
 function videjais(x, y) {
   return (x + y) / 2;
 }
+console.log(videjais(5,9));
 
-console.log(videjais(5,5));
 function lielakais(x,y) {
   if(x > y){
     return x;
   }
   if(y > x){
     return y;
-    
   }
-
-  }
-  console.log(lielakais(5,2));
+}
+console.log(lielakais(5,2));
   
-
 function pi() {
   return a = 3.14159265359;
 }
