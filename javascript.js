5fd174a4b263d35d1d98ee51e339cabe5bcb9722
renars <renars>
2025-12-15 14:34:40 +0200

    lielākais

--- javascript.js
+++ javascript.js
@@ -4,3 +4,17 @@ function kvadarat(x) {
   return x * x;
 }
 console.log(x);
+
+function lielakais(x,y) {
+  if(x > y){
+    return x;
+  }
+  if(y > x){
+    return y;
+    
+  }
+
+  }
+  console.log(5,2);
+  
+
